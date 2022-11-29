24277d0de2a1eb75a0734d69803e1a8e6a147b9f
Ahmed <ahmed.abdelbary@student.manchester.ac.uk>
2022-11-29 18:31:38 +0000

    Update hex files

--- hex/HexLogic.py
+++ hex/HexLogic.py
@@ -5,53 +5,51 @@ Board class.
 Board data:
   1=white, -1=black, 0=empty
   first dim is column , 2nd is row:
-     pieces[1][7] is the square in column 2,
-     at the opposite end of the board in row 8.
+	 pieces[1][7] is the square in column 2,
+	 at the opposite end of the board in row 8.
 Squares are stored and manipulated as (x,y) tuples.
 x is the column, y is the row.
 '''
 #TODO
 class Board():
-    def __init__(self, n):
-        "Set up initial board configuration."
-        self.n = n
-        # Create the empty board array.
-        self.pieces = [None]*self.n
-        for i in range(self.n):
-            self.pieces[i] = [0]*self.n
+	def __init__(self, n):
+		"Set up initial board configuration."
+		self.n = n
+		self.legal_moves = set()
+		self.can_swap = True
+		
+		# Create the empty board array.
+		self.pieces = [None]*self.n
+		for i in range(self.n):
+			self.pieces[i] = [0]*self.n
+			
+		# Initialize the legal moves.
+		for y in range(self.n):
+			for x in range(self.n):
+				self.legal_moves.add((x,y))
+		
+	# add [][] indexer syntax to the Board
+	def __getitem__(self, index): 
+		return self.pieces[index]
 
-    # add [][] indexer syntax to the Board
-    def __getitem__(self, index): 
-        return self.pieces[index]
+	def get_legal_moves(self, color):
+		"""Returns all the legal moves for the given color.
+		(1 for white, -1 for black
+		"""
+		return list(self.legal_moves) #TODO: Maybe do not return as list
 
-    def get_legal_moves(self, color):
-        """Returns all the legal moves for the given color.
-        (1 for white, -1 for black
-        """
-        moves = set()  # stores the legal moves.
+	def has_legal_moves(self):
+		"""Returns True if has legal move else False
+		"""
+		# Get all empty locations.
+		return len(self.legal_moves) > 0
 
-        # Get all empty locations.
-        for y in range(self.n):
-            for x in range(self.n):
-                if self[x][y] == 0:
-                    moves.add((x, y))
-        return list(moves)
-
-    def has_legal_moves(self):
-        """Returns True if has legal move else False
-        """
-        # Get all empty locations.
-        for y in range(self.n):
-            for x in range(self.n):
-                if self[x][y] == 0:
-                    return True
-        return False
-
-    def execute_move(self, move, color):
-        """Perform the given move on the board; flips pieces as necessary.
-        color gives the color pf the piece to play (1=white,-1=black)
-        """
-        (x,y) = move
-        assert self[x][y] == 0
-        self[x][y] = color
+	def execute_move(self, move, color):
+		"""Perform the given move on the board; flips pieces as necessary.
+		color gives the color pf the piece to play (1=white,-1=black)
+		"""
+		(x,y) = move
+		assert self[x][y] == 0
+		self[x][y] = color
+		self.legal_moves.remove(move)
 
