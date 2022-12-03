768e0781149fd11fff7f2f3e2932daccbf2e1218
Ahmed <ahmed.abdelbary@student.manchester.ac.uk>
2022-12-03 08:57:38 +0000

    hotfix (remove line)

--- NNModel.py
+++ NNModel.py
@@ -27,6 +27,5 @@ class NNModel():
         self.pi = Dense(self.action_size, activation='softmax', name='pi')(s_fc2)   # batch_size x self.action_size
         self.v = Dense(1, activation='tanh', name='v')(s_fc2)                    # batch_size x 1
         
-        with MirroredStrategy().scope():
         self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
         self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args["lr"]))
